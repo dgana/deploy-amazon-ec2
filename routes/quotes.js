344310332f2735ed40e09c71290d0980d8dbb264
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-03-14 11:49:51 +0700

    add seed data

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const Quotes = require('../models/quotes')
+const seedQuotes = require('../seeder/quotes')
+const mongoose = require('mongoose')
 
 router.get('/', (req, res) => {
   Quotes.find((err, quotes) => {
@@ -9,14 +11,16 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
-  var newQuote = Quotes({
-    quote: req.body.quote
-  });
-  newQuote.save((err, quote) => {
-    if (err) throw err;
-    res.json(quote)
-  });
+router.post('/seed', (req, res) => {
+  Quotes.remove(err => {
+      if (err) res.status(500).send(err)
+      else console.log('Dropped collection: quotes')
+    })
+  Quotes.create(seedQuotes, (err, quotes) => {
+    if (err) res.status(500).send(err)
+    else res.send(quotes)
+  })
 });
+
 
 module.exports = router;
---
+++ seeder/quotes.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    quote: "The present time has one advantage over every other - it is our own"
+  },
+  {
+    quote: "Love is not love that alters when it alteration finds"
+  },
+  {
+    quote: "Sorrowful and great is the artist's destiny"
+  },
+  {
+    quote: "While I relish our warm months, winter forms our character and brings out our best"
+  },
+  {
+    quote: "There are lots of people who mistake their imagination for their memory"
+  },
+]
